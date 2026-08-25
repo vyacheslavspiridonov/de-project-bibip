ec7f208fbccec4b68bb40a98cf65341c27ed80e9
Вячеслав <vyacheslavspiridonov1001@yandex.ru>
2026-08-25 23:12:40 +0300

    rename files

--- src/For tests/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-path = Path(os.path.dirname(os.path.realpath(__file__)))
-
-sys.path.append(os.path.abspath(path.parent.absolute()))
-sys.path.append(os.path.abspath(path.parent.absolute().joinpath("src")))
